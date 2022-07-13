9e368a00b54c378754b53dfa87a3c2a0cdde1869
Inari13066 <karpova13066@gmail.com>
2022-07-13 17:32:45 +0400

    feat: task ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let string = Math.abs(n).toString();
+
+    let number = string.slice(-1);
+    for (let i = 1; i < string.length; i++) {
+        number += string.slice(-(i + 1), -i);
+    }
+
+    return number;
+};
